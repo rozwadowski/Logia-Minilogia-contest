6f57fb5302545091ccaeb5ffade43fba0695fb83
rozwadowski <siotsonek@gmail.com>
2018-10-21 23:24:21 +0200

    Add files via upload

---
+++ 17.3.2.abc.py
@@ -0,0 +1,31 @@
+
+def good(napis):
+	for i in range(len(napis)):
+		if ( napis[i] == "c" or napis[i] == "z" ) and "n" in napis[i+1:]:
+			return False
+		if napis[i] == "z" and "c" in napis[i+1:]:
+			return False
+	return True
+
+def check(napis,wynik,n):
+	if n != 0:
+		if good(napis):
+			wynik.append(napis)
+		else:
+			for i in range(len(napis)):
+				check(napis[:i]+napis[i+1:],wynik,n-1)		
+
+def abc(napis):
+	wynik = []
+	i = 1
+	while len(wynik) == 0:
+		check(napis,wynik,i)
+		i += 1
+	return len(napis)-len(wynik[0])
+
+
+		
+		
+#print( abc("zzczzc") )
+print( abc('nncnnbffbbbccczzzcz') )
+print( abc('zzzznnnnz') )
